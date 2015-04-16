44ca842f60843beaed5dae73a41e95f8aa93525d
h1994st <h1994st@gmail.com>
2015-04-16 20:35:28 +0800

    done

--- FM/src/edu/tongji/Main.java
+++ FM/src/edu/tongji/Main.java
@@ -7,7 +7,9 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		FaultManagement fm = new FaultManagement();
+		fm.generateWarningMessage("test");
+		fm.generateWarningMessage("test2", "./warning/");
 	}
 
 }
