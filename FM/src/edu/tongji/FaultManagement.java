57464c183e759bfc0f315788330d1760067c2efa
h1994st <h1994st@gmail.com>
2015-04-16 16:48:18 +0800

    init

---
+++ FM/src/edu/tongji/FaultManagement.java
@@ -0,0 +1,11 @@
+package edu.tongji;
+
+public class FaultManagement {
+	public void generateWarningMessage(String message) {
+		this.generateWarningMessage(message, "");
+	}
+	
+	public void generateWarningMessage(String message, String filepath) {
+		
+	}
+}
